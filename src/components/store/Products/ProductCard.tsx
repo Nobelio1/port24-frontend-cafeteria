d564b38ef03b7e75f662332f4b1e75338b03cc68
Gino Alejandro <ginoalejandro2304@gmail.com>
2024-11-04 15:38:46 -0500

    termina el diseño de las card

--- src/components/store/Products/ProductCard.tsx
+++ src/components/store/Products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { ProductCardProps } from "@/components/product/ProductCard";
 import Image from "next/image";
+import { FaPlus } from "react-icons/fa";
 
 export default function ProductCard({
   id,
@@ -8,13 +9,34 @@ export default function ProductCard({
   description,
   price,
 }: ProductCardProps) {
-  console.log(id,description,price);
+  console.log(id)
   return (
-    <div className="h-[160px] w-full flex bg-white rounded-md overflow-hidden drop-shadow-5xl">
-      <div>
-        <Image src={img} alt={title} width={160} height={160} />
+    <div className="h-[160px] w-full flex bg-white rounded-md overflow-hidden drop-shadow-5xl relative">
+      <div className="min-w-[160px]">
+        <Image
+          src={img}
+          alt={title}
+          width={160}
+          height={160}
+          className="object-contain"
+        />
       </div>
-      <div></div>
+      <div className="flex flex-col justify-between w-[60%] sm:w-56 h-40 p-3.5">
+        <div className="text-ellipsis overflow-hidden break-words">
+          <span className="block font-semibold text-base cursor-pointer">
+            {title}
+          </span>
+          <span className="mt-0.5 text-xs whitespace-pre-wrap text-ellipsis line-clamp-3">
+            {description}
+          </span>
+        </div>
+        <div className="text-sm">
+          <div>S/ {price}</div>
+        </div>
+      </div>
+      <div className="absolute p-2 right-2.5 bottom-2.5 bg-black rounded-full">
+        <FaPlus color="white" size={14} />
+      </div>
     </div>
   );
 }
