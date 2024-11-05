5d506ad4cfeab697bedab3f905f337282e711b09
Gino Alejandro <ginoalejandro2304@gmail.com>
2024-11-04 19:39:02 -0500

    implementa seccion de la canasta de las compras

--- src/components/store/cart/ShoppingCart.tsx
+++ src/components/store/cart/ShoppingCart.tsx
@@ -1,8 +1,39 @@
+import { SlLocationPin } from "react-icons/sl";
 
 export default function ShoppingCart() {
   return (
-    <div>
-      <h1>Shopping Cart</h1>
+    <div className="flex flex-col h-[74vh] drop-shadow-5xl rounded-lg bg-white ml-2 mt-6 sticky px-2 py-4 top-5">
+      <div className="px-3 font-semibold text-lg">Tu Carrito</div>
+      <div className="flex-initial flex-col h-20 align-center py-4 px-2 border-b dark:border-zinc-300">
+        <div className="inline-flex text-sm border-2 items-center transition ease-in-out duration-150 bg-gray-100 text-gray-700 border-gray-100 cursor-pointer hover:bg-gray-200 hover:border-gray-200 rounded-full px-4 py-0 h-full w-full overflow-hidden">
+          <div className="flex-1 font-semibold">
+            <div className="flex items-center gap-2 px-2 truncate">
+              <div>
+                <SlLocationPin size={20} />
+              </div>
+              <div className="flex flex-col">
+                <span className="text-xs font-semibold">
+                  Ingrese su dirección
+                </span>
+                <span className="flex text-xs font-normal italic">
+                  Lo antes posible
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 overflow-y-auto">
+        <div className="full-w flex-row justify-center text-center p-6">
+          <h5 className="flex-1 break-normal text-base font-semibold">
+            ¡Arma tu carrito ahora!
+          </h5>
+          <p className="flex-1 break-normal font-light text-sm">
+            Los productos que agregues aparecerán aquí
+          </p>
+        </div>
+      </div>
+      <div className="flex-initial flex-col align-center border-t dark:border-zinc-300"></div>
     </div>
   );
 }
